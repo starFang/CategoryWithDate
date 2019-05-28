e1cb35e8705d3a86dc003e6202fdadba6b901990
fangyb <fangyb@taikanglife.com>
2019-05-28 23:10:29 +0800

    init Demo

---
+++ TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by Star童话 on 2019/5/28.
+//  Copyright © 2019 STAR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TestDemo
+//
+//  Created by Star童话 on 2019/5/28.
+//  Copyright © 2019 STAR. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
